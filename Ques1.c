6ada0f5841a28d4998715052b515b72319411bec
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2023-06-27 13:09:16 +0530

    Delete Ques1.c

--- Ques1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    typedef double num[3];
-    num array[5] = {1,2,3,4,5,6};
-    printf("%u", sizeof(array));
-    printf(" %.2f", array[0][1]);
-    return 0;
-}
